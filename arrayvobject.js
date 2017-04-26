1048b26f0cfbdee008e4b1209ef7e5447e87336f
JasmineD93 <Jasmine031611@gmail.com>
2017-04-26 15:59:34 -0500

    Add files via upload

---
+++ arrayvobject.js
@@ -0,0 +1,20 @@
+//New Array
+var cats = ["Garrus","Abby","Momo"];
+
+//retrieve data
+console.log(cats[2]);
+
+//updating items
+cats[1] = "Abbey";
+
+/////////////////////////////////////////////////
+
+
+//New Object
+var cat = {
+	name: "Momo",
+	colour: "Orange"
+};
+
+//retrieve data
+console.log(cat.colour);
